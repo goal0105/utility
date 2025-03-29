ab43e19b0ee44f4e1c72baca689901fb625f1009
futureman0105 <future.star.1216@gmail.com>
2025-03-29 07:05:21 -0700

    add app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import sys
+from download_best import Download
+
+# Example Usage
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python download.py <URL>")
+        sys.exit(1)
+
+    video_url = sys.argv[1]
+    downloader = Download()  # Customize output dir as needed
+    # while(1):
+    downloader.download_from_url(video_url)
