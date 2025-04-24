25f1bf92b02b16091c4d361bb89f803e6afe8b41
futureman0105 <future.star.1216@gmail.com>
2025-04-24 08:16:59 -0700

    testing

--- download-youtube/app.py
+++ download-youtube/app.py
@@ -4,12 +4,18 @@ from downloader import Download
    
 # Example Usage  
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python download.py <URL>")
-        sys.exit(1)
+    # if len(sys.argv) < 2:
+    #     print("Usage: python download.py <URL>")
+    #     sys.exit(1)
 
-    video_url = sys.argv[1]
+    # video_url = sys.argv[1]
     downloader = Download()  # Customize output dir as needed
-    # while(1):
-    downloader.download_from_url(video_url)
+    
+    counter = 0
+    while(1):
+        counter += 1
+        print(f"Downloading {counter}")
+        downloader.download_from_url("https://www.youtube.com/watch?v=1MwSoB0gnM4")
+ 
+
     
